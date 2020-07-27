8198bcf0783bc4ce1aac68e3573ed90af4cf0a1e
Kareem Elsawah <alsawahkareem@gmail.com>
2020-07-27 13:42:48 +0200

    Enhanced printing

--- Task 3, Binary Search/main.c
+++ Task 3, Binary Search/main.c
@@ -4,8 +4,10 @@
 int main()
 {
     uint32_t arr[10] = {1,2,3,9,4,3,5,8,0,1};
-
-    uint8_t index = binarySearch(arr,10,10);
+    printf("Searching in: ");
+    print_array(arr,10);
+    printf("Searching for: %d\n", 9);
+    uint8_t index = binarySearch(arr,10,9);
 
     // Not found vs found condition and print
     if(index==NULL){
